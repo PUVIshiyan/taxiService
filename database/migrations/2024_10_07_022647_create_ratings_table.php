a8d91745686abe0d0a947d2d2bfd48c7c902f552
PUVIshiyan <164120062+PUVIshiyan@users.noreply.github.com>
2024-10-07 08:00:52 +0530

    driverLocation , rating model and migration create

---
+++ database/migrations/2024_10_07_022647_create_ratings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ride_id')->constrained('rides')->onDelete('cascade'); // Link to the ride
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // User who left the rating
+            $table->tinyInteger('rating')->unsigned(); // Rating score (1-5)
+            $table->text('feedback')->nullable(); // Optional feedback
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ratings');
+    }
+};
